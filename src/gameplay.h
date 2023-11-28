58aeb7a87473ab046d3270c5eed92c801aeaeb17
Benjamin Bremer <bwb1113@rit.edu>
2023-11-28 17:21:14 -0500

    Created some basic functions and variables in gameplay.h to handle gameplay functionality.

--- src/gameplay.h
+++ src/gameplay.h
@@ -5,8 +5,48 @@
 #ifndef DOMINION_GAME_GAMEPLAY_H
 #define DOMINION_GAME_GAMEPLAY_H
 
+int actions;
+int buys;
+int current_player;
+int total_players;
 
 class gameplay {
+
+    void add_action() {
+
+        actions ++;
+
+    }
+
+    void add_buy() {
+
+        buys ++;
+
+    }
+
+    void use_action() {
+
+        action --;
+
+    }
+
+    void use_buy() {
+
+        buys --;
+
+    }
+
+    void switch_player() {
+
+        current_player ++;
+
+        if(current_player > total_players) {
+
+            current_player = 1;
+
+        }
+
+    }
 
 };
 
