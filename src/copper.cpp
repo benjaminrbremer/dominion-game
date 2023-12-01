5634c29910aa33c70b355c6c0c0b34a5c600369f
Benjamin Bremer <bwb1113@rit.edu>
2023-12-01 08:08:03 -0500

    Added copper and estates, needed to finish the initialization of gameplay. Just need to figure out exactly how a card's properties are going to be assigned to it.

---
+++ src/copper.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Benjamin Bremer on 12/1/23.
+//
+
+#include "card.h"
+
+class copper{
+
+    name = "Copper";
+    type = TREASURE;
+    properties_list = new void*[1];
+    // properties_list needs to add one to the number of treasure
+
+};
---
+++ src/estate.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Benjamin Bremer on 12/1/23.
+//
+
+#include "card.h"
+
+class estate {
+
+    name = "estate";
+    type = VICTORY_POINT;
+    // Victory points have no value in-game
+    properties_list = NULL;
+
+};
